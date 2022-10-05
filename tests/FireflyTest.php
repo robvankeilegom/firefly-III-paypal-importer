9cd4f4c971f28e49c38aefcfae092ded898f689e
Rob Van Keilegom <robvankeilegom@hotmail.com>
2022-10-05 21:39:52 +0200

    :white_check_mark: Add firefly tests

---
+++ tests/FireflyTest.php
@@ -0,0 +1,74 @@
+<?php
+
+use App\Firefly;
+use App\Models\Payer;
+use App\Models\Transaction;
+
+beforeEach(function () {
+    $this->firefly = new Firefly();
+});
+
+it('can push an expense', function () {
+    $payer = Payer::factory()->create();
+
+    $transaction = Transaction::factory()
+        ->for($payer)
+        ->expense()
+        ->create();
+
+    $response = $this->firefly->push($transaction);
+
+    expect($response)->not->toBeFalse();
+});
+
+it('can push a transaction with an existing unknown payer', function () {
+    $payer = Payer::factory()->create();
+
+    $transaction = Transaction::factory()
+        ->for($payer)
+        ->expense()
+        ->create();
+
+    $response = $this->firefly->push($transaction);
+
+    // Clear the firefly data
+    $payer->firefly_expense_id = null;
+    $payer->firefly_revenue_id = null;
+    $payer->save();
+
+    $transaction = Transaction::factory()
+        ->for($payer)
+        ->expense()
+        ->create();
+
+    $response = $this->firefly->push($transaction);
+
+    expect($response)->not->toBeFalse();
+});
+
+it('can push the same transaction twice', function () {
+    $payer = Payer::factory()->create();
+
+    $transaction = Transaction::factory()
+        ->for($payer)
+        ->expense()
+        ->create();
+
+    $response = $this->firefly->push($transaction);
+    $response = $this->firefly->push($transaction);
+
+    expect($response)->not->toBeFalse();
+});
+
+it('can push a refund', function () {
+    $payer = Payer::factory()->create();
+
+    $transaction = Transaction::factory()
+        ->for($payer)
+        ->refund()
+        ->create();
+
+    $response = $this->firefly->push($transaction);
+
+    expect($response)->not->toBeFalse();
+});
